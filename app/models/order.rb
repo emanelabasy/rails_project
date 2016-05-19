52bc49ca1370e82758b0946f9e061c995e6fa79e
emyelabasy <egad86@yahoo.com>
2016-05-19 10:10:45 +0200

    eman elabasy write relation of model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  has_many :orders
+  has_many :invites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_many :groups
+  has_many :friends
+  has_many :orders
+  has_many :invites
+  has_many :amount_orders   
 end
